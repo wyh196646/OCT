b894f64a09486ff217fc12c5dd8b0fd6cf39fc43
wyh196646 <1966463192@qq.com>
2021-10-25 16:06:23 +0800

    new

---
+++ creater_tool.py
@@ -0,0 +1,39 @@
+class ProposalCreator:
+
+
+    #def __init__(self):#空构造函数直接让类自动创建
+
+    def __call__(self, loc, score,
+                 anchor, img_size, scale=1.):
+        roi[:, slice(0, 4, 2)] = np.clip(
+            roi[:, slice(0, 4, 2)], 0, img_size[0])
+        roi[:, slice(1, 4, 2)] = np.clip(
+            roi[:, slice(1, 4, 2)], 0, img_size[1])
+
+        min_size = self.min_size * scale
+        hs = roi[:, 2] - roi[:, 0]
+        ws = roi[:, 3] - roi[:, 1]
+        keep = np.where((hs >= min_size) & (ws >= min_size))[0]
+        roi = roi[keep, :]
+        score = score[keep]
+
+
+        order = score.ravel().argsort()[::-1]
+        if n_pre_nms > 0:
+            order = order[:n_pre_nms]
+        roi = roi[order, :]
+        score = score[order]
+
+        # Apply nms (e.g. threshold = 0.7).
+        # Take after_nms_topN (e.g. 300).
+
+        # unNOTE: somthing is wrong here!
+        # TODO: remove cuda.to_gpu
+        keep = nms(
+            torch.from_numpy(roi).cuda(),
+            torch.from_numpy(score).cuda(),
+            self.nms_thresh)
+        if n_post_nms > 0:
+            keep = keep[:n_post_nms]
+        roi = roi[keep.cpu().numpy()]
+        return roi
